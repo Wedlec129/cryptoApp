09d96878f3a9b0ecb92973c3ffb2bc09b9b6d2a5
Wedlec129 <boryasokolov@gmail.com>
2023-09-22 09:34:56 +0300

    Initial Commit

---
+++ cryptoApp/cryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoAppApp.swift
+//  cryptoApp
+//
+//  Created by Борух Соколов on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct cryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
